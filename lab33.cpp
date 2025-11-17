f69f8356be0e267320846dd3768c20aafadb6474
chrizxzayax <chmolina629@hotmail.com>
2025-11-16 21:16:17 -0800

    start of the int main

--- lab33.cpp
+++ lab33.cpp
@@ -9,3 +9,13 @@
 #include "Car.h"
 
 using namespace std;
+
+const int DEFAULT_NUM_LANES = 4;
+const int TOTAL_P = 20;
+
+int main(int argc, char** argv){
+
+    unsigned seed = (unsigned) time(nullptr);
+
+    int total_p = TOTAL_P;
+}
