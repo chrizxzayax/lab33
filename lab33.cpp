7080a0dec20d5bd97a695ccd0a3e84dffffe807d
chrizxzayax <chmolina629@hotmail.com>
2025-11-16 21:05:26 -0800

    firstly i add the main includes ill likely use

---
+++ lab33.cpp
@@ -0,0 +1,11 @@
+// // comsc 210 | lab33 | Christian Molina
+
+#include <iostream>
+#include <deque>
+#include <iomanip>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include "Car.h"
+
+using namespace std;
